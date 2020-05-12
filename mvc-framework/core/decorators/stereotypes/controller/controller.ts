cbd484c8eca1a8ef12cbdf51e5db4d15a8e8cd0b
Andres Pirela <andreespirela@outlook.com>
2020-05-11 23:46:53 -0400

    pathRoute to baseRoute. It makes more sense.

--- mvc-framework/core/decorators/stereotypes/controller/controller.ts
+++ mvc-framework/core/decorators/stereotypes/controller/controller.ts
@@ -4,18 +4,18 @@ import { ComponentRegistryContext } from "../../../../../main-core/components-re
 import { ComponentUtils } from "../../../../../main-core/utils/componentUtils.ts";
 import { ApplicationContext } from "../../../../../main-core/application-context/mandarineApplicationContext.ts";
 
-export const Controller = (pathRoute?: string, name?: string): Function => {
+export const Controller = (baseRoute?: string, name?: string): Function => {
     return (target: any, methodName: string, index: number) => {
         let className: string = ReflectUtils.getClassName(target);
             if(ApplicationContext.getInstance().getComponentsRegistry().exist(className)) {
 
                 let objectContext: ComponentRegistryContext = ApplicationContext.getInstance().getComponentsRegistry().get(className);
                 let controllerComponent:ControllerComponent = <ControllerComponent> objectContext.componentInstance;
-                controllerComponent.setRoute(pathRoute);
+                controllerComponent.setRoute(baseRoute);
 
                 ApplicationContext.getInstance().getComponentsRegistry().update(className, objectContext);
             } else {
-                ComponentUtils.createControllerComponent(name, { pathRoute: pathRoute }, target);
+                ComponentUtils.createControllerComponent(name, { pathRoute: baseRoute }, target);
             }
     };
 };
