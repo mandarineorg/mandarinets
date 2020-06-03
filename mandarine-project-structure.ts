36b0d47760def02201469fa727da8c44f522bb8e
Andres Pirela <andreespirela@outlook.com>
2020-06-03 16:15:51 -0400

    no message

--- mandarine-project-structure.ts
+++ mandarine-project-structure.ts
@@ -1,8 +1,30 @@
-import { CommonUtils } from "./main-core/utils/commonUtils.ts";
+const mandarineDefaultConfig = `
+{
+    "mandarine": {
+        "server": {
+            "host":"0.0.0.0",
+            "port":8080,
+            "responseType":"text/html"
+        },
+        "templateEngine": {
+            "path":"./src/main/resources/templates",
+            "engine":"ejs"
+        }
+    }
+}`;
 
-const mandarineDefaultConfig = CommonUtils.readFile('./defaults/config.json');
+const mandarineDefaultAppFile = `
+import { MandarineCore } from "https://deno.land/x/mandarinets/mod.ts";
 
-const mandarineDefaultAppFile = CommonUtils.readFile('./defaults/app.ts');
+const services = [];
+const middleware = [];
+const repositories = [];
+const configurations = [];
+const components = [];
+const otherModules = [];
+
+new MandarineCore().MVC().run();
+`;
 
 export const structure = {
     folders: [
