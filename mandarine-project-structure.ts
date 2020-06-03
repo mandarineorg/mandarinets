64b642cdda5bc91d62419e8f23feb126126e4c35
Andres Pirela <andreespirela@outlook.com>
2020-06-02 21:42:15 -0400

    no message

---
+++ mandarine-project-structure.ts
@@ -0,0 +1,13 @@
+export const structure = {
+    folders: [
+        "/test",
+        "/src",
+        "/src/main",
+        "/src/main/mandarine",
+        "/src/main/resources",
+        "/src/main/resources/templates"
+    ],
+    files: [
+        "/src/main/resources/properties.json"
+    ]
+}
