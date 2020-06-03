55df9486a2803705996e47e9945c22a0b08db400
Andres Pirela <andreespirela@outlook.com>
2020-06-02 22:21:12 -0400

    no message

--- mandarine-project-structure.ts
+++ mandarine-project-structure.ts
@@ -1,3 +1,31 @@
+const mandarineDefaultConfig = `
+{
+    "mandarine": {
+        "server": {
+            "host":"0.0.0.0",
+            "port":8080,
+            "responseType":"text/html"
+        },
+        "templateEngine": {
+            "path":"./src/main/resources/templates",
+            "engine":"ejs"
+        }
+    }
+}`;
+
+const mandarineDefaultAppFile = `
+import { MandarineCore } from "https://deno.land/x/mandarinets/mod.ts";
+
+const services = [];
+const middleware = [];
+const repositories = [];
+const configurations = [];
+const components = [];
+const otherModules = [];
+
+new MandarineCore().MVC().run();
+`;
+
 export const structure = {
     folders: [
         "/test",
@@ -8,33 +36,7 @@ export const structure = {
         "/src/main/resources/templates"
     ],
     files: {
-        "/src/main/resources/properties.json": `
-        {
-            "mandarine": {
-                "server": {
-                    "host":"0.0.0.0",
-                    "port":8080,
-                    "responseType":"text/html"
-                },
-                "templateEngine": {
-                    "path":"./src/main/resources/templates",
-                    "engine":"ejs"
-                }
-            }
-        }
-        `,
-        "/src/main/mandarine/app.ts": `
-
-        import { MandarineCore } from "https://deno.land/x/mandarinets/mod.ts";
-
-        const services = [];
-        const middleware = [];
-        const repositories = [];
-        const configurations = [];
-        const components = [];
-        const otherModules = [;
-        
-        new MandarineCore().MVC().run();
-        `
+        "/src/main/resources/properties.json": mandarineDefaultConfig,
+        "/src/main/mandarine/app.ts": mandarineDefaultAppFile
     }
-}
+};
