28544b8a66d96a5d49da49b26abf1363c36a375b
Andres Pirela <andreespirela@outlook.com>
2020-06-23 09:34:48 -0400

    Mandarine dependent modules are now located inside mandarine's project.

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { Application, Cookies, Request, Router, Context, Middleware, isHttpError, Status } from "https://deno.land/x/oak@v5.3.1/mod.ts";
+export { Application, Cookies, Request, Router, Context, Middleware, isHttpError, Status } from "https://raw.githubusercontent.com/mandarineorg/mandarinets-modules/master/oak/5.3.1/mod.ts";
