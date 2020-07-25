6a315d3d8d0509e54ef6ab79a8428d7fc233b713
Andres Pirela <andreespirela@outlook.com>
2020-07-24 21:15:44 -0400

    Support for environmental variables to assign where mandarine should look for mandarine' properties files

---
+++ main-core/MandarineEnvConstants.ts
@@ -0,0 +1,6 @@
+export class MandarineEnvironmentalConstants {
+
+    public static readonly MANDARINE_JSON_FILE: string = "MANDARINE_JSON_FILE";
+    public static readonly MANDARINE_PROPERTY_FILE: string = "MANDARINE_PROPERTY_FILE";
+
+}
