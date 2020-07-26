3dde40a2984711107c74dc901e0f707731099235
Andres Pirela <andreespirela@outlook.com>
2020-07-26 14:15:09 -0400

    small bug fix

--- mvc-framework/mandarineMVC.ts
+++ mvc-framework/mandarineMVC.ts
@@ -71,12 +71,13 @@ export class MandarineMVC {
         .use(starter.getRouter().routes())
         .use(starter.getRouter().allowedMethods())
         .use(ResourceHandlerMiddleware())
-        .use(async (ctx, next) => {
-            await next();
-        });
 
         this.oakMiddleware.forEach((middleware) => {
             app = app.use(middleware);
+        });
+
+        app = app.use(async (ctx, next) => {
+            await next();
         });
 
         app.addEventListener("error", (event) => {
