311086850d807a4a82c7a920a45f19217fc37e32
Andres Pirela <andreespirela@outlook.com>
2020-08-13 21:26:01 -0400

    Adding copyright to unit test

--- tests/unit-tests/utils/commonUtils_test.ts
+++ tests/unit-tests/utils/commonUtils_test.ts
@@ -1,3 +1,5 @@
+// Copyright 2020-2020 The Mandarine.TS Framework authors. All rights reserved. MIT license.
+
 import { Test, DenoAsserts, Orange } from "../../mod.ts";
 import { CommonUtils } from "../../../main-core/utils/commonUtils.ts";
 
