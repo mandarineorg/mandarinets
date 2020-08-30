f0fced2f7b7d5b11c366379716a2263b1f97ff87
Andres Pirela <>
2020-08-30 15:18:44 -0400

    Session container docs and small fix for decorators

--- orm-core/core/proxys/ormCoreDecoratorProxy.ts
+++ orm-core/core/proxys/ormCoreDecoratorProxy.ts
@@ -27,7 +27,7 @@ export class ORMCoreDecoratorProxy {
 
     public static registerColumnDecorator(targetClass: any, decoratorOptions: Mandarine.ORM.Entity.Decorators.Column, propertyKey: string) {
         var propertyType = Reflect.getMetadata("design:type", targetClass, propertyKey);
-        var propertyTypeName = propertyType.name;
+        var propertyTypeName = propertyType?.name;
 
         let currentTargetAnnotations: Array<any> = Reflect.getMetadataKeys(targetClass);
 
@@ -42,7 +42,9 @@ export class ORMCoreDecoratorProxy {
 
         decoratorOptions.fieldName = propertyKey;
 
-        if(propertyTypeName === 'String' && decoratorOptions.type == (undefined || null)) {
+        if(!propertyTypeName) {
+            decoratorOptions.type = Types.VARCHAR;
+        } else if(propertyTypeName === 'String' && decoratorOptions.type == (undefined || null)) {
             decoratorOptions.type = Types.VARCHAR;
         } else if(propertyTypeName === 'Boolean' && decoratorOptions.type == (undefined || null)) {
             decoratorOptions.type = Types.BOOLEAN;
