57fc38f410a680486466213a170c38fc73e25fd0
Andres Pirela <>
2020-10-15 09:45:53 -0400

    Advances

--- rust-core/database/drivers/postgres/src/lib.rs
+++ rust-core/database/drivers/postgres/src/lib.rs
@@ -4,58 +4,82 @@ extern crate lazy_static;
 #[macro_use]
 extern crate serde_json;
 
-use postgres::{Client, NoTls};
+use tokio_postgres::{Config as PGConfig, NoTls};
+use futures::{Future, FutureExt};
 use deno_core::plugin_api::{Interface, Op, ZeroCopyBuf};
+use deadpool_postgres::{Manager, ManagerConfig, Pool, RecyclingMethod};
 use serde::{Deserialize, Serialize};
 use std::{
-    collections::HashMap,
+    clone::Clone,
+    pin::Pin,
+    result::Result,
     sync::{
-        atomic::{AtomicUsize},
-        Mutex, MutexGuard,
+        Mutex,
     },
 };
+mod commands;
+mod util;
 
 lazy_static! {
-    static ref CLIENTS: Mutex<HashMap<usize, Client>> = Mutex::new(HashMap::new());
-    static ref NEXT_CLIENT_ID: AtomicUsize = AtomicUsize::new(0);
+    static ref PG_CONFIG: Mutex<PGConfig> = Mutex::new(PGConfig::new());
+    static ref PG_POOL: Mutex<Option<Pool>> = Mutex::new(None);
+    static ref PG_MANAGER: Mutex<Option<Manager<NoTls>>> = Mutex::new(None);
+    static ref IS_CONNECTED: Mutex<Option<bool>> = Mutex::new(Some(false));
+}
+
+pub type Buf = Box<[u8]>;
+
+#[derive(Serialize)]
+pub struct SyncResult<T>
+where
+    T: Serialize,
+{
+    data: Option<T>,
+    error: Option<String>,
+}
+
+#[derive(Serialize)]
+pub struct AsyncResult<T>
+where
+    T: Serialize,
+{
+    command_type: CommandType,
+    data: Option<T>,
+    error: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 pub enum CommandType {
     Connect,
-    Close,
-    Execute,
-    BatchExecute,
-    Query,
-    TransactionCreate,
-    TransactionCommit
+    Calculate
 }
 
-#[derive(Clone)]
-pub struct Command {
-    args: CommandArgs,
-    data: Vec<ZeroCopyBuf>,
-}
-
-#[derive(Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct CommandArgs {
     command_type: CommandType,
     client_id: Option<usize>,
 }
 
-impl Command {
-    fn new(args: CommandArgs, data: Vec<ZeroCopyBuf>) -> Command {
-        Command { args, data }
-    }
-
-    fn get_client(&self) -> Client {
-        get_client(self.args.client_id.unwrap())
-    }
+#[derive(Serialize, Deserialize, Clone)]
+pub struct PoolConf<M, P> {
+    manager: M,
+    pool: P,
 }
 
-impl CommandArgs {
-    fn new(data: &[u8]) -> CommandArgs {
-        serde_json::from_slice(data).unwrap()
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Command {
+    args: CommandArgs,
+    #[serde(skip)]
+    data: Vec<ZeroCopyBuf>
+}
+
+impl Command {
+    fn new(header: &[u8], data: Vec<ZeroCopyBuf>) -> Command {
+        let comand_args_from_header: CommandArgs = serde_json::from_slice(header).unwrap();
+        Command {
+            args : comand_args_from_header,
+            data : data
+        }
     }
 }
 
@@ -64,15 +88,12 @@ pub fn deno_plugin_init(interface: &mut dyn Interface) {
     interface.register_op("mandarine_postgres_plugin", op_command);
 }
 
-pub(crate) fn get_client(client_id: usize) -> Client {
-    let map: MutexGuard<HashMap<usize, Client>> = CLIENTS.lock().unwrap();
-    map.get(&client_id).unwrap().clone()
-}
-
 fn op_command(_interface: &mut dyn Interface, zero_copy: &mut [ZeroCopyBuf]) -> Op {
     let (first, rest) = zero_copy.split_first().unwrap();
-
-    let result: Vec<u8> = "x".to_string().into_bytes();
-    let result_box: Box<[u8]> = result.into_boxed_slice();
-    Op::Sync(result_box)
+    let args: Command = Command::new(first, rest.to_vec());
+    let args2: Command = args.clone();
+    match args2.args.command_type {
+        CommandType::Connect => util::sync_op(commands::connect, args2),
+        CommandType::Calculate => util::async_op(commands::calculate, args2)
+    }
 }
