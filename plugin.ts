03f124fafa7dedd0ec0790eed157ab7e9e86de89
Andres Pirela <>
2020-10-16 11:28:35 -0400

    connection

--- plugin.ts
+++ plugin.ts
@@ -8,6 +8,20 @@ Deno.core.setAsyncHandler(mandarine_postgres_plugin, (msg: Uint8Array) => {
     console.log("Gotten from dispatch", new TextDecoder().decode(msg));
     return msg;
 });
+
+//@ts-ignore
+let x = Deno.core.dispatch(mandarine_postgres_plugin, 
+    encode.encode(JSON.stringify({
+    command_type: "Connect",
+    client_id: 0
+})), encode.encode(JSON.stringify({
+    host: "127.0.0.1",
+    username: "postgres",
+    password: "Changeme1",
+    dbname: "mandarine",
+    port: 5432
+})));
+
 console.log("Before process");
 // @ts-ignore
 let lol: Promise<number> = Deno.core.dispatch(mandarine_postgres_plugin, 
