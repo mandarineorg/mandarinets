00a28ed1d020f266a123df0824e7c9e1d15da53b
Andres Pirela <>
2020-10-27 12:06:33 -0400

    Docs & mod.ts for postgres driver

---
+++ rust-core/database/drivers/postgres/ts-src/mod.ts
@@ -0,0 +1,8 @@
+// Copyright 2020-2020 The Mandarine.TS Framework authors. All rights reserved. MIT license.
+
+export { PgClient } from "./pgClient.ts";
+export { PgManager } from "./pgManager.ts";
+export type { Configuration } from "./pgManager.ts";
+export { QueryResult } from "./queryResult.ts";
+export type { IQueryResult } from "./queryResult.ts";
+export type { Column, Row } from "./rows.ts";
