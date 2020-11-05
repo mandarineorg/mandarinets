96afaac67a3055dc0a61e475c238f29a298de8d3
andreespirela <andreespirela@outlook.com>
2020-11-05 13:57:09 -0500

    Copyright header

--- tests/unit-tests/authentication_test.ts
+++ tests/unit-tests/authentication_test.ts
@@ -1,3 +1,5 @@
+// Copyright 2020-2020 The Mandarine.TS Framework authors. All rights reserved. MIT license.
+
 import { ApplicationContext } from "../../main-core/application-context/mandarineApplicationContext.ts";
 import { Authenticator } from "../../main-core/mandarine-native/security/authenticatorDefault.ts";
 import { Mandarine } from "../../main-core/Mandarine.ns.ts"
