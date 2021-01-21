498b11027f191b5022041744ebaadc284e678ce6
andreespirela <andreespirela@outlook.com>
2021-01-21 16:15:12 -0500

    Oak 6.4.1

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 // Copyright 2020-2020 The Mandarine.TS Framework authors. All rights reserved. MIT license.
 
 // @ts-ignore
-export { Application, Cookies, Request, Router, Context, Middleware, isHttpError, Status, FormDataReader, send, Response } from "https://raw.githubusercontent.com/mandarineorg/mandarinets-modules/master/oak/6.3.0/mod.ts";
+export { Application, Cookies, Request, Router, Context, Middleware, isHttpError, Status, FormDataReader, send, Response } from "https://raw.githubusercontent.com/mandarineorg/mandarinets-modules/master/oak/6.4.1/mod.ts";
