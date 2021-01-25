71d56ea18b4401e7588a1255ed9df9ce9487a533
andreespirela <andreespirela@outlook.com>
2021-01-25 09:57:16 -0500

    Updating automatic builder

--- tests/app-builder-tests/controller.ts
+++ tests/app-builder-tests/controller.ts
@@ -1,6 +1,6 @@
 // Copyright 2020-2020 The Mandarine.TS Framework authors. All rights reserved. MIT license.
 
-import { Controller, GET, Service} from "https://deno.land/x/mandarinets@v2.2.0/mod.ts";
+import { Controller, GET, Service} from "https://deno.land/x/mandarinets/mod.ts";
 
 @Controller()
 export class MyController {
