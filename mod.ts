5d1d6977080e89ec3aa87d512d362ebc8495e1fc
andreespirela <60560109+andreespirela@users.noreply.github.com>
2021-04-29 14:05:24 -0400

    feat(#289):Expose new functionalities in mod.ts
     feat(#289):Expose new functionalities in mod.ts

--- mod.ts
+++ mod.ts
@@ -59,6 +59,13 @@ export { Authenticator } from "./main-core/mandarine-native/security/authenticat
 export { Microservice, MSOnOpen, MSOnClose, MSOnError, MSOnMessage, MSClose } from "./main-core/decorators/microservices/microservice.ts";
 export { MicroserviceUserManager } from "./main-core/mandarine-native/microservices/microserviceUserManager.ts";
 
+export { Scheduled, Timer } from "./main-core/decorators/tasks/taskScheduler.ts";
+
+export { WebSocketClient, WebSocketServer, WSOnOpen, WSOnClose, WSOnError, WSOnMessage, WSSend, WSClose } from "./main-core/decorators/websockets/webSocket.ts"
+
+export { MongoDBService } from "./orm-core/nosql/mongoDbService.ts";
+
+export { EventListener } from "./main-core/decorators/stereotypes/events/dom/eventListener.ts"
 /*
 OpenAPI
 */
